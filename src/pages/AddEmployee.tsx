fe20516f4e858012a03093419efd0b8282cb6adf
Iypan Amdin <manieee1126@gmail.com>
2025-11-03 13:14:35 +0530

    first commit

--- src/pages/AddEmployee.tsx
+++ src/pages/AddEmployee.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from 'react'
 import { useNavigate, useSearchParams } from 'react-router-dom'
 import { employeeService } from '../services/employeeService'
-import { Employee } from '../types/employee'
 import Navbar from '../components/Navbar'
 import toast from 'react-hot-toast'
 
